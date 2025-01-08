a7cb3886cddee1556026087658083e0add15ffaa
TrevorEngland23 <trevor.england.523@gmail.com>
2025-01-08 06:51:28 -0800

    Add Controller for the buy now button: PART F (decrement by 1 logic with products)

--- src/main/java/com/example/demo/controllers/BuyNowController.java
+++ src/main/java/com/example/demo/controllers/BuyNowController.java
@@ -1,4 +1,44 @@
 package com.example.demo.controllers;
+import com.example.demo.domain.Product;
+import com.example.demo.repositories.ProductRepository;
+import com.example.demo.service.ProductService;
+import com.example.demo.service.ProductServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class BuyNowController {
+    @Autowired
+    private final ProductService productService;
+
+    public BuyNowController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/buyNow")
+    public String buyNow(@RequestParam("productID") int productID) {
+       Product product = productService.findById(productID);
+
+       if (product != null && product.getInv() > 0) {
+           product.setInv(product.getInv() - 1);
+           productService.save(product);
+           return "successfulpurchase";
+       } else {
+           return "unsuccessfulpurchase";
+       }
+
+//        Product product = repo.findById(productID);
+//        int inv = product.getInv();
+//
+//        if (inv > 0) {
+//            product.setInv(inv - 1);;
+//            repo.save(product);
+//            return "successfulpurchase";
+//        } else {
+//            return "unsuccessfulpurchase";
+//        }
+    }
 }
