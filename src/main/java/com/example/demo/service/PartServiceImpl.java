e9f3eb96390f22bdfe979a0f6125c6e51630824b
TrevorEngland23 <trevor.england.523@gmail.com>
2025-01-10 12:04:43 -0800

    no change: edited while debugging and reverted back

--- src/main/java/com/example/demo/service/PartServiceImpl.java
+++ src/main/java/com/example/demo/service/PartServiceImpl.java
@@ -1,11 +1,8 @@
 package com.example.demo.service;
-
 import com.example.demo.domain.Part;
-import com.example.demo.domain.Product;
 import com.example.demo.repositories.PartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 import java.util.Optional;
 
@@ -56,9 +53,7 @@ public class PartServiceImpl implements PartService{
 
     @Override
     public void save(Part thePart) {
-            this.partRepository.save(thePart);
             partRepository.save(thePart);
-
     }
 
     @Override
